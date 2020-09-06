79bd7ec0c5ea68b87e7a3878c3a5c2642db63a0f
MrSohux <55574445+mrsohux@users.noreply.github.com>
2020-09-06 16:24:06 +0530

    Create CountDigit.c

---
+++ CountDigit.c
@@ -0,0 +1,35 @@
+/*
+Write a program which accept one number from user and count number of ON (1) bits in it without using % and / operator.
+Input : 11
+Output : 3
+
+*/
+
+
+#include<stdio.h>
+int Countbit(int iNo)
+{
+	int iCnt=0;
+	
+	while(iNo)
+	{
+		iCnt +=iNo & 1;
+		iNo <<1; //   iNo>>=1
+	}
+	return iCnt;
+}
+int main()
+{
+	int iValue=0;
+	
+	int iRet=0;
+	
+	printf("Enter Number");
+	scanf("%d",&iValue);
+	
+	iRet=Countbit(iValue);
+	
+	printf("%d",iRet);
+	
+	return 0;
+}
